6de8a2a73ac324f17cefdf5990b3dcea993946e6
Roland Li <liqi618@gmail.com>
2025-07-08 21:28:47 -0700

    face towards mouse

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -23,6 +23,11 @@ public class Player : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePos - transform.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
     void FixedUpdate()
